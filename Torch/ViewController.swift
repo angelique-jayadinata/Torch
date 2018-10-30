82115b977dbf7476b813ab1459cefd18c9e61ced
agustin susilowati <agustin@agustins-macbook-pro.local>
2018-10-30 14:24:13 +0700

    Initial Commit

---
+++ Torch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Torch
+//
+//  Created by agustin susilowati on 30/10/18.
+//  Copyright © 2018 Angie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
